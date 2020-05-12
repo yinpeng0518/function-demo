74b25919f0e6b065e129621e68cdde68f7141c97
尹鹏 <yinpeng@fy-data.com>
2020-05-12 11:10:52 +0800

    初始化ssm-send

---
+++ ssm-send/src/main/java/com/yp/ssmsend/SsmSendApplication.java
@@ -0,0 +1,13 @@
+package com.yp.ssmsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmSendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmSendApplication.class, args);
+    }
+
+}
---
+++ ssm-send/src/test/java/com/yp/ssmsend/SsmSendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yp.ssmsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmSendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

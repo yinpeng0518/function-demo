dc080f3e0e7d2c490468c570bb5050d72e5b1d32
尹鹏 <yinp0312@163.com>
2020-05-13 16:36:06 +0800

    添加图片验证码功能代码

---
+++ captcha-image/src/main/java/com/yp/captchaimage/CaptchaImageApplication.java
@@ -0,0 +1,13 @@
+package com.yp.captchaimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchaImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaptchaImageApplication.class, args);
+    }
+
+}
---
+++ captcha-image/src/test/java/com/yp/captchaimage/CaptchaImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yp.captchaimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptchaImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
